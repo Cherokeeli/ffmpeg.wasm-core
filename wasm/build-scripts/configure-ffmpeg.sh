934a2def6736efa06ab2856f349e4e33c78cbb17
CharlynQuan <charlynquan@tencent.com>
2022-06-08 23:57:44 +0800

    feat custom config build

--- wasm/build-scripts/configure-ffmpeg.sh
+++ wasm/build-scripts/configure-ffmpeg.sh
@@ -4,24 +4,24 @@ set -euo pipefail
 source $(dirname $0)/var.sh
 
 FLAGS=(
-  "${FFMPEG_CONFIG_FLAGS_BASE[@]}"
-#  --enable-gpl            # required by x264
-  --enable-nonfree        # required by fdk-aac
-#  --enable-zlib           # enable zlib
-#  --enable-libx264        # enable x264
-#  --enable-libx265        # enable x265
-#  --enable-libvpx         # enable libvpx / webm
-  --enable-libwavpack     # enable libwavpack
-  --enable-libmp3lame     # enable libmp3lame
-  --enable-libfdk-aac     # enable libfdk-aac
-#  --enable-libtheora      # enable libtheora
-  --enable-libvorbis      # enable libvorbis
-#  --enable-libfreetype    # enable freetype
-  --enable-libopus        # enable opus
-#  --enable-libwebp        # enable libwebp
-#  --enable-libass         # enable libass
-#  --enable-libfribidi     # enable libfribidi
-  # --enable-libaom         # enable libaom
+  -I. -I./fftools -I$BUILD_DIR/include
+  -Llibavcodec -Llibavdevice -Llibavfilter -Llibavformat -Llibavresample -Llibavutil -Llibass -Llibpostproc -Llibswscale -Llibswresample -L$BUILD_DIR/lib
+  -Wno-deprecated-declarations -Wno-pointer-sign -Wno-implicit-int-float-conversion -Wno-switch -Wno-parentheses -Qunused-arguments
+  -lavdevice -lavfilter -lavformat -lavcodec -lswresample -lavutil -lm -lwavpack -lmp3lame -lfdk-aac -lvorbis -lvorbisenc -lvorbisfile -logg -lopus
+  fftools/ffmpeg_opt.c fftools/ffmpeg_filter.c fftools/ffmpeg_hw.c fftools/cmdutils.c fftools/ffmpeg.c
+  -s USE_SDL=2                                  # use SDL2
+  -s INVOKE_RUN=0                               # not to run the main() in the beginning
+  -s EXIT_RUNTIME=1                             # exit runtime after execution
+  -s MODULARIZE=1                               # use modularized version to be more flexible
+  -s EXPORT_NAME="createFFmpegCore"             # assign export name for browser
+  -s EXPORTED_FUNCTIONS="[_main]"  # export main and proxy_main funcs
+  -s EXPORTED_RUNTIME_METHODS="[FS, cwrap, ccall, setValue, writeAsciiToMemory]"   # export preamble funcs
+  -s INITIAL_MEMORY=2146435072                  # 64 KB * 1024 * 16 * 2047 = 2146435072 bytes ~= 2 GB
+  -s ASSERTIONS=1
+  --pre-js wasm/src/pre.js
+  --post-js wasm/src/post.js
+  $OPTIM_FLAGS
+  ${EXTRA_FLAGS[@]}
 )
 echo "FFMPEG_CONFIG_FLAGS=${FLAGS[@]}"
 emconfigure ./configure "${FLAGS[@]}"
